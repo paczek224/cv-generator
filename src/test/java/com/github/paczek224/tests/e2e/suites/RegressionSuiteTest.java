b77b202005c83aafc46063185eb6cd24f27c9fec
Lukasz PACZEK <lukasz.paczek-external@lyreco.com>
2026-06-13 21:39:45 +0200

    updated regression suite allure property

--- src/test/java/com/github/paczek224/tests/e2e/suites/RegressionSuiteTest.java
+++ src/test/java/com/github/paczek224/tests/e2e/suites/RegressionSuiteTest.java
@@ -10,5 +10,6 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.github.paczek224.tests.e2e")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 public class RegressionSuiteTest {
 }
